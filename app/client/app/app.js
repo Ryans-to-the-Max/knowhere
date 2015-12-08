0fa5d4247c4b19ff81101902c44bfc7b1135799e
Brian Nguyen <brianacnguyen@gmail.com>
2015-12-08 14:51:26 -0800

    completed basic angular routing, and factories and controllers for results, hotels, attractions, and restaurants

--- app/client/app/app.js
+++ app/client/app/app.js
@@ -13,9 +13,21 @@ angular.module('travel', [
       templateUrl: 'app/landing/landing.html',
       controller: 'LandingController'
     })
-    .when('/', {
-      templateUrl: 'app/landing/landing.html',
-      controller: 'LandingController'
+    .when('/results', {
+      templateUrl: 'app/results/results.html',
+      controller: 'ResultsController'
+    })
+    .when('/attractions', {
+      templateUrl: 'app/attractions/attractions.html',
+      controller: 'AttractionsController'
+    })
+    .when('/hotels', {
+      templateUrl: 'app/hotels/hotels.html',
+      controller: 'HotelsController'
+    })
+    .when('/restaurants', {
+      templateUrl: 'app/restaurants/restaurants.html',
+      controller: 'RestaurantsController'
     })
     .otherwise({
       redirectTo: '/'
