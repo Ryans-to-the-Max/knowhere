e7fcc7e8fc571f1ca814c3b91c49e164826fc89e
mgaff <mgaffney123@gmail.com>
2015-12-08 19:34:00 -0500

    (feat) basic passport setup

--- app/server/server.js
+++ app/server/server.js
@@ -1,7 +1,12 @@
 var bodyParser = require('body-parser');
 var express = require('express');
-var morgan = require('morgan');
+var http = require('http');
 var path = require('path');
+var cookieParser = require('cookie-parser');
+var passport = require('passport');
+var flash    = require('connect-flash');
+var session  = require('express-session');
+var morgan = require('morgan');
 
 // Server routers:
 var index = require(path.join(__dirname, 'routes/index'));
@@ -10,19 +15,45 @@ var dest = require(path.join(__dirname, 'routes/dest'));
 
 var app = express();
 
-// Log requests:
-app.use(morgan('dev'));
-
-// Client Route:
+//Client Route
 app.use(express.static(path.join(__dirname, '../client')));
 
+// App config:
 app.use(bodyParser.json());
 // parse application/x-www-form-urlencoded:
 app.use(bodyParser.urlencoded({ extended: false }));
+// needed for auth
+app.use(cookieParser());
+// required for passport
+app.use(session({ secret: 'tripAppIsAmazing' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+require('./config/passport')(passport); // pass passport for configuration
 
 // Server routing:
 app.use('/api', index);
 app.use('/api/dest', dest);
 
+//Authentication Routing
+app.post('/login', passport.authenticate('local-login', {
+  successRedirect : '/', 
+  failureRedirect : '/login', // redirect back to the login page if there is an error
+  failureFlash : true // allow flash messages
+}));
 
-module.exports = app;
+app.post('/signup', passport.authenticate('local-signup', {
+  successRedirect : '/',
+  failureRedirect : '/signup', // redirect back to the signup page if there is an error
+  failureFlash : true // allow flash messages
+}));
+
+
+// Spin up server:
+var port = process.env.PORT || '3000';
+app.set('port', port);
+
+var server = http.createServer(app);
+
+server.listen(port);
+console.log('Server now listening on port ' + port);
