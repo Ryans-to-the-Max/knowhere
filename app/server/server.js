db5f7901a7c5164aae2d29892070550fcd46fe17
mgaff <mgaffney123@gmail.com>
2015-12-10 15:29:15 -0500

    (fix) deleted duplicated code

--- app/server/server.js
+++ app/server/server.js
@@ -84,11 +84,4 @@ app.get('/logout', function(req, res){
 });
 
 
-// Spin up server:
-var port = process.env.PORT || '3000';
-app.set('port', port);
-
-var server = http.createServer(app);
-
-server.listen(port);
-console.log('Server now listening on port ' + port);
+module.exports = app;
