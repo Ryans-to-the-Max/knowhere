c10317d9c1bf079a7f210c0e2a579a019fc6767e
Brian Nguyen <brianacnguyen@gmail.com>
2015-12-09 16:05:14 -0800

    modified landing page with requirements and added a function to clean up input

--- app/client/app/landing/landing.js
+++ app/client/app/landing/landing.js
@@ -3,8 +3,24 @@ angular.module('travel.landing', [])
 .controller('LandingController', function ($scope, $location, CurrentInfo, City) {
   $scope.data = {};
   $scope.sendData = function() {
-    CurrentInfo.origin.name = $scope.data.origin;
-    CurrentInfo.destination.name = $scope.data.destination;
+    CurrentInfo.origin.name = cleanInput($scope.data.origin);
+    CurrentInfo.destination.name = cleanInput($scope.data.destination);
     $location.path('/attractions');
   };
 });
+
+var removeSpace = function(string) {
+	var result = string;
+	var arrayofString = string.split("");
+	if (arrayofString[arrayofString.length - 1] === " ") {
+		arrayofString.pop();
+		result = removeSpace(arrayofString.join(""));
+		return result;
+	} else {
+		return result;
+	}; 
+};
+
+var cleanInput = function(string) {
+	return removeSpace(string).replace(/\s/g, '-').toLowerCase();
+};
