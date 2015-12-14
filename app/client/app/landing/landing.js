278951210aaa4bae4decf494dd67e260d289f35d
Zachary R. Smith <ZacharySmith4989@gmail.com>
2015-12-14 17:50:09 -0500

    (fix) Fix "kebabCase" function in LandingController

--- app/client/app/landing/landing.js
+++ app/client/app/landing/landing.js
@@ -2,27 +2,15 @@ angular.module('travel.landing', [])
 
 .controller('LandingController', function ($scope, $window, $location, CurrentInfo, City) {
   $scope.data = {};
+
+  var kebabCase = function (string) {
+    return string.trim().replace(/\s+/g, '-').toLowerCase();
+  };
+
   $scope.sendData = function() {
-    CurrentInfo.origin.name = cleanInput($scope.data.origin);
-    CurrentInfo.destination.name = cleanInput($scope.data.destination);
-    var dest = CurrentInfo.destination.name; 
-    $window.sessionStorage.setItem('knowhere', dest);
+    CurrentInfo.origin.name = kebabCase($scope.data.origin);
+    CurrentInfo.destination.name = kebabCase($scope.data.destination);
+    $window.sessionStorage.setItem('knowhere', CurrentInfo.destination.name);
     $location.path('/attractions');
   };
 });
-
-var removeSpace = function(string) {
-	var result = string;
-	var arrayofString = string.split("");
-	if (arrayofString[arrayofString.length - 1] === " ") {
-		arrayofString.pop();
-		result = removeSpace(arrayofString.join(""));
-		return result;
-	} else {
-		return result;
-	} 
-};
-
-var cleanInput = function(string) {
-	return removeSpace(string).replace(/\s/g, '-').toLowerCase();
-};
--- spec/unit/controllersSpec.js
+++ spec/unit/controllersSpec.js
@@ -1,50 +1,75 @@
-// 'use strict';
+'use strict';
 
 describe('Knowhere angular controllers', function () {
 
   beforeEach(module('travel'));
 
   describe('AttractionsController', function () {
-    var ctrl, scope;
+    var $httpBackend, ctrl, scope;
 
     beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
       $httpBackend = _$httpBackend_;
       $httpBackend.expectGET('/api/dest')
-          .respond('hello');
+          .respond('city info');
       $httpBackend.expectGET('/api/dest/places')
-          .respond('hello');
+          .respond('attractions info');
 
       scope = $rootScope.$new();
       ctrl = $controller('AttractionsController', { $scope: scope });
     }));
 
-    it('should work', function () {
+    it('should get attractions', function () {
+      expect(scope.attractions).toEqual(null);
+
       $httpBackend.flush();
-      expect(scope.attractions).toEqual('hello');
-      // setTimeout(function () {
-      //   $httpBackend.flush();
-      //   expect(scope.attractions).toEqual('hello');
-      //   done();
-      // }, 1000);
-      // scope
-      //   .getAttractions()
-      //   .then(function () {
-      //     expect(scope.attractions).toEqual('hello');
-      //     done();
-      //   })
+
+      expect(scope.attractions).toEqual('attractions info');
     });
+  });
 
-    // it('should work', inject(function ($controller) {
-    //   var scope = { };
-    //   var ctrl = $controller('AttractionsController', { $scope: scope });
+  describe('HotelsController', function () {
+    var $httpBackend, ctrl, scope;
 
-    //   // expect(scope.attractions).toEqual(null);
+    beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('/api/dest')
+          .respond('city info');
+      $httpBackend.expectGET('/api/dest/hotels')
+          .respond('hotels info');
 
-    //   setTimeout(function () {
-    //     expect(scope.attractions).toEqual('foo');  
-    //   }, 0);
-    //   // scope.getAttractions();
-      
-    // }));
+      scope = $rootScope.$new();
+      ctrl = $controller('HotelsController', { $scope: scope });
+    }));
+
+    it('should get hotels', function () {
+      expect(scope.hotels).toEqual(null);
+
+      $httpBackend.flush();
+
+      expect(scope.hotels).toEqual('hotels info');
+    });
+  });
+
+  describe('RestaurantsController', function () {
+    var $httpBackend, ctrl, scope;
+
+    beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('/api/dest')
+          .respond('city info');
+      $httpBackend.expectGET('/api/dest/rests')
+          .respond('restaurants info');
+
+      scope = $rootScope.$new();
+      ctrl = $controller('RestaurantsController', { $scope: scope });
+    }));
+
+    it('should get restaurants', function () {
+      expect(scope.restaurants).toEqual(null);
+
+      $httpBackend.flush();
+
+      expect(scope.restaurants).toEqual('restaurants info');
+    });
   });
 });
