8a9e667a1b7e47c7f0cde077cd2f3e3d5b52fb8f
Zachary R. Smith <ZacharySmith4989@gmail.com>
2015-12-16 15:43:21 -0500

    (refactor) Start fixing LandingController

--- app/client/app/landing/landing.js
+++ app/client/app/landing/landing.js
@@ -2,44 +2,29 @@ angular.module('travel.landing', [])
 
 .controller('LandingController', function ($scope, $window, $state, $rootScope, CurrentInfo, City, Groups, Util) {
   var data;
-  $scope.data = {};
+  $scope.data = { };
 
   $scope.sendData = function() {
     $window.sessionStorage.setItem('knowhere', Util.transToPermalink($scope.data.destination));
     $rootScope.currentUser = $rootScope.currentUser || "anonymous";
+    $scope.data.group = $scope.data.group || "anonymous";
 
-    if ($scope.data.group === undefined) {
-      data = {
-        groupName: "anonymous",
-        userInfo: $rootScope.currentUser,
-        destination: $window.sessionStorage.getItem('knowhere')
-      };
-      Groups.createGroup(data);
-      Groups.getGroups("anonymous")
-      .then(function(groupsInfo){
-        groupsInfo.forEach(function(group){
-          if (group.title === "anonymous") {
-            group.destination = $window.sessionStorage.getItem('knowhere'); 
-            $rootScope.currentGroup = group;
-          }
-        });
-      });
-    } else {
-      data = {
-        groupName: $scope.data.group,
-        userInfo: $rootScope.currentUser,
-        destination: $window.sessionStorage.getItem('knowhere')
-      };
-      Groups.createGroup(data);
-      Groups.getGroups($rootScope.currentUser)
+    Groups.createGroup({
+      groupName: $scope.data.group,
+      userInfo: $rootScope.currentUser,
+      destination: $window.sessionStorage.getItem('knowhere')
+    });
+
+    // FIXME We need to give createGroup some time before getGroups will work...promisify this
+    // TODO We should not be searching through groups on client side
+    Groups.getGroups($rootScope.currentUser)
         .then(function(groupsInfo){
-          groupsInfo.forEach(function(group){
+          groupsInfo.some(function(group){
             if (group.title === $scope.data.group) {
               $rootScope.currentGroup = group;
             }
           });
         });
-    }
 
     $state.go('results');
   };
