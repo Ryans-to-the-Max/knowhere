5d3cd491098252663c824bc209c64d1d3e0d529e
Zachary R. Smith <ZacharySmith4989@gmail.com>
2015-12-16 22:37:32 -0500

    (fix) Fix Groups services URL

--- app/client/app/services/services.js
+++ app/client/app/services/services.js
@@ -8,7 +8,7 @@ angular.module('travel.services', [])
   var getGroups = function(userId){
     return $http({
       method: 'GET',
-      url: '/api/user/groups',
+      url: '/api/group',
       params: { userId: userId }
     })
     .then(function(resp){
